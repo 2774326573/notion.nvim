129ff76c78b91efb488b237d148766b42b7e3b80
onewu <jinxinjinqi@outlook.com>
2025-11-05 19:44:15 +0800

    增强文本对象和段落块，添加plain_text属性以支持原始文本内容

--- lua/notion/parser.lua
+++ lua/notion/parser.lua
@@ -33,6 +33,7 @@ local function text_object(text)
   return {
     type = "text",
     text = { content = text },
+    plain_text = text,
     annotations = annotations_defaults(),
   }
 end
@@ -195,6 +196,7 @@ local function paragraph_block(text, annotations)
         {
           type = "text",
           text = { content = text },
+          plain_text = text,
           annotations = ann,
         },
       },
