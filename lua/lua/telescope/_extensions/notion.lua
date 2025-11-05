0288c9bf5837fb042fe900ce49f9188b0775c8ff
onewu <jinxinjinqi@outlook.com>
2025-11-05 20:57:26 +0800

    删除借鉴文件

--- lua/lua/telescope/_extensions/notion.lua
+++
@@ -1,7 +0,0 @@
-return require("telescope").register_extension {
-    setup = function(ext_config, config)
-    end,
-    exports = {
-        notion = require("notion.telescope").openMenuTelescope
-    },
-}
